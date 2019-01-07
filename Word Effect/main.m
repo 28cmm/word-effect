7c562b33fbfbe777372896ed422265d5dfafd7ae
28cmm <joshuafang1998@gmail.com>
2019-01-07 13:58:35 -0800

    Initial Commit

---
+++ Word Effect/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effect
+//
+//  Created by Yilei Huang on 2019-01-07.
+//  Copyright © 2019 Joshua Fanng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
